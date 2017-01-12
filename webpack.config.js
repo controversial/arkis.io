f0f4eb527576ed5d2eb21442ded5609d19640915
Luke Deen Taylor <luke@deentaylor.com>
2017-01-12 02:16:04 -0500

    Suppress UglifyJS warnings

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, compress: { warnings: false } }),
   ],
 
 
