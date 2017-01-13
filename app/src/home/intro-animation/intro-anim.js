4b0acbec31c82f2f32ef55e131216807adb7cb1b
Luke Deen Taylor <luke@deentaylor.com>
2017-01-13 00:10:39 -0500

    Add method for running code after logo interaction animation is finished
    Delays interaction event by amount of time the resulting animations take to play

--- app/src/home/intro-animation/intro-anim.js
+++ app/src/home/intro-animation/intro-anim.js
@@ -1,1 +1,9 @@
 require('./intro-anim.sass');
+
+const isTouchDevice = 'ontouchstart' in document.documentElement;
+const ev = isTouchDevice ? 'touchend' : 'click';
+const delay = isTouchDevice ? 1050 : 550;
+
+document.getElementById('corners').addEventListener(ev, () => setTimeout(() => {
+  console.log('Animation finished');
+}, delay));
