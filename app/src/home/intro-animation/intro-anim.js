704b099635aced353f80e0b4546dd71c0f303226
Luke Deen Taylor <luke@deentaylor.com>
2017-01-14 09:58:43 -0500

    Use element classes instead of element ids in some places

--- app/src/home/intro-animation/intro-anim.js
+++ app/src/home/intro-animation/intro-anim.js
@@ -4,6 +4,6 @@ const isTouchDevice = 'ontouchstart' in document.documentElement;
 const ev = isTouchDevice ? 'touchend' : 'click';
 const delay = isTouchDevice ? 1050 : 550;
 
-document.getElementById('corners').addEventListener(ev, () => setTimeout(() => {
+document.getElementById('animated-logo').addEventListener(ev, () => setTimeout(() => {
   document.getElementById('animated-logo').className = 'clicked';
 }, delay));
