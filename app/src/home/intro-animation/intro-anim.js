3890a3eb2246a10c0134cc25ce86620210ed183b
Luke Deen Taylor <luke@deentaylor.com>
2017-01-14 14:43:17 -0500

    Don't allow interacting with logo until initial build is finished

--- app/src/home/intro-animation/intro-anim.js
+++ app/src/home/intro-animation/intro-anim.js
@@ -4,6 +4,19 @@ const isTouchDevice = 'ontouchstart' in document.documentElement;
 const ev = isTouchDevice ? 'touchend' : 'click';
 const delay = isTouchDevice ? 1050 : 550;
 
-document.getElementById('animated-logo').addEventListener(ev, () => setTimeout(() => {
-  document.getElementById('animated-logo').className = 'clicked';
-}, delay));
+// Make the logo interactable after the animation is finished
+setTimeout(() => {
+  const logo = document.getElementById('animated-logo');
+  const cornersDiv = document.getElementById('corners');
+  cornersDiv.className = 'interactable';
+
+  // Click event triggers animations on the elements
+
+  cornersDiv.addEventListener(ev, () => setTimeout(() => {
+    logo.className = 'clicked';
+    // Set display: none on logo
+    setTimeout(() => {
+      logo.style.display = 'none';
+    }, 1000);
+  }, delay));
+}, 3750);
