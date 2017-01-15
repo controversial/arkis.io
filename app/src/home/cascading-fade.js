989e0f3321dfbc6ff70340aa9dcab4b5d7225c7b
Luke Deen Taylor <luke@deentaylor.com>
2017-01-15 12:28:28 -0500

    Cascading fade JS

--- app/src/home/cascading-fade.js
+++ app/src/home/cascading-fade.js
@@ -1,3 +1,13 @@
+const contentMain = document.getElementById('content-main');
+const fadeElements = contentMain.querySelectorAll('[data-fade]');
+
 module.exports = function cascadeFadeIn() {
-  document.getElementById('content-main').className = 'visible';
+  contentMain.className = 'visible';
+
+  for (let i = 0; i < fadeElements.length; i += 1) {
+    const elem = fadeElements[i];
+    setTimeout(() => {
+      elem.style.opacity = 1;
+    }, elem.getAttribute('data-fade') * 1000);
+  }
 };
