50287aa56d79fdcb017c1e0b946cae438058d6cb
Luke Deen Taylor <luke@deentaylor.com>
2017-01-15 23:53:12 -0500

    Plan out background animation

---
+++ app/src/home/background-anim.js
@@ -0,0 +1,10 @@
+// Background animation of a maze being procedurally generated from the center out. Uses
+// a breadth-first traversal algorithm for the sake of a pleasing generation visualization, even
+// though depth-first traversal produces a better-looking final output. This animation was inspired
+// by https://bost.ocks.org/mike/algorithms/#maze-generation
+
+// (TODO) Logic for generating a maze (bfs) in real time
+module.exports.MazeGenerator = class MazeGenerator {};
+
+// (TODO) Renders a grid onto a canvas
+module.exports.GridRenderer = class GridRenderer {};
