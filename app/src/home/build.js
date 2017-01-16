f7168d8dda6c21f8c9f05c000a3379a09f587e3c
Luke Deen Taylor <luke@deentaylor.com>
2017-01-16 18:06:56 -0500

    Don't prevent scroll

--- app/src/home/build.js
+++ app/src/home/build.js
@@ -1,8 +1,6 @@
 const contentMain = document.getElementById('content-main');
 const fadeElements = contentMain.querySelectorAll('[data-build-delay]');
 
-document.body.style.overflow = 'hidden';
-
 module.exports = function cascadeFadeIn() {
   contentMain.className = 'visible';
 
@@ -12,8 +10,4 @@ module.exports = function cascadeFadeIn() {
       elem.className += ' visible';
     }, elem.getAttribute('data-build-delay') * 1000);
   }
-
-  setTimeout(() => {
-    document.body.style.overflow = '';
-  }, 4000);
 };
