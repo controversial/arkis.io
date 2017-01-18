3d24e7cadfa5f5615433864c63560277e4ea8efa
Luke Deen Taylor <luke@deentaylor.com>
2017-01-17 20:22:32 -0500

    Scroll effect for top bar

--- app/src/home/scroll.js
+++ app/src/home/scroll.js
@@ -1,5 +1,32 @@
+// Top bar changes from white to white with scroll, as the background changes
+
+function adjustTopBar(scroll) {
+  const topBar = document.getElementsByClassName('top-bar')[0];
+
+  // Scroll distance at which everything will switch from black to white
+  const threshold = window.innerWidth > window.innerHeight
+                        ? window.innerHeight * 1.5
+                        : window.innerHeight;
+
+  // Background color gradually changes until threshold
+  const gray = Math.floor(255 - (Math.min(scroll / threshold, 1) * 255));
+  topBar.style.backgroundColor = `rgba(${gray}, ${gray}, ${gray}, .1)`;
+
+  // Text color changes suddenly at threshold
+  if (scroll > threshold) { // Text color
+    topBar.style.color = 'black';
+  } else {
+    topBar.style.color = 'white';
+  }
+}
+
+
+// Main scroll handler
+
+
 function scrollHandler() {
-  console.log(window.scrollY);
+  const scroll = window.scrollY;
+  adjustTopBar(scroll);
 }
 
 window.addEventListener('scroll', scrollHandler);
