3f6d2ad0bdd6fa07505971ec6d282f5bfcf62d36
Luke Deen Taylor <luke@deentaylor.com>
2017-01-17 20:28:00 -0500

    Fix portrait scrollbar color threshold

--- app/src/home/scroll.js
+++ app/src/home/scroll.js
@@ -1,5 +1,6 @@
 // Scroll effects for the page
 
+
 // Top bar text changes from white to black after scrolling a certain amount
 function adjustTopBar(scroll) {
   const topBar = document.getElementsByClassName('top-bar')[0];
@@ -7,7 +8,7 @@ function adjustTopBar(scroll) {
   // Scroll distance at which everything will switch from black to white
   const threshold = window.innerWidth > window.innerHeight
                         ? window.innerHeight * 1.5
-                        : window.innerHeight;
+                        : window.innerHeight * 0.95;
 
   // Text color changes suddenly at threshold
   if (scroll > threshold) {
