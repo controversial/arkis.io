3111841b6e8f4fb6415028ffdbe23c5be6f29025
Luke Deen Taylor <luke@deentaylor.com>
2017-01-19 17:20:08 -0500

    Play with canvas gradient masking

--- app/src/home/mist.js
+++ app/src/home/mist.js
@@ -5,8 +5,17 @@ window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight * 0.8;
 
+  ctx.fillStyle = 'white';
+  ctx.font = `${canvas.height}px Arial`;
+  ctx.fillText('Hi', 10, canvas.height);
+
+
+  // Make everything faded with a gradient
+
+  ctx.globalCompositeOperation = 'destination-in';
+
   const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-  gradient.addColorStop(0, 'rgba(255, 255, 255, 0.2)');
+  gradient.addColorStop(0, 'rgba(255, 255, 255, 0)');
   gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
