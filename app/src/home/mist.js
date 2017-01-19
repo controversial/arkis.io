63cbc9d284b6a338004d92d46ee1aa623d5080c2
Luke Deen Taylor <luke@deentaylor.com>
2017-01-19 00:21:35 -0500

    Experiment with canvas gradients

--- app/src/home/mist.js
+++ app/src/home/mist.js
@@ -1,2 +1,13 @@
 const canvas = document.getElementById('mist');
 const ctx = canvas.getContext('2d');
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight * 0.8;
+
+  const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(0, 'rgba(255, 255, 255, 0.2)');
+  gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
