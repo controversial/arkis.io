1b627e36c6e604d9f72acb72e9d260d755f85331
Luke Deen Taylor <luke@deentaylor.com>
2017-01-22 14:49:06 -0500

    Add JSDoc-style comments

--- app/src/home/mist.js
+++ app/src/home/mist.js
@@ -1,5 +1,6 @@
 // Helper functions
 
+
 // My [].fill polyfill
 if (!Array.prototype.fill) {
   /* eslint-disable no-extend-native */
@@ -21,8 +22,17 @@ function randint(low, high) {
 }
 
 
-// Basic particle physics (moving and bouncing)
+/** Implements basic particle physics (moving and bouncing) */
 class Particle {
+  /**
+   * Create a new particle in a simulation.
+   * Particles have positions and velocities, and remember which renderer they're bound to.
+   * @param {ParticleRenderer} renderer - the ParticleRender that the particle belongs to.
+   * @param {number} x - the initial X position of the particle
+   * @param {number} y - the initial Y position of the particle
+   * @param {number} vx - the initial X velocity of the particle
+   * @param {number} vy - the initial Y velocity of the particle
+   */
   constructor(renderer, x, y, vx, vy) {
     this.renderer = renderer;
     this.x = x;    // X position on canvas
@@ -31,6 +41,7 @@ class Particle {
     this.vy = vy;  // Y velocity
   }
 
+  /** Advance the simulation by one step */
   step() {
     // Bounce particles when they hit the edges of the canvas
     if (this.x < 0 || this.x >= this.renderer.width()) {
@@ -44,18 +55,25 @@ class Particle {
     this.y += this.vy;
   }
 
+  /** Cause the current particle to bounce in the X direction */
   bounceX() {
     this.vx = -1 * this.vx;
   }
 
+  /** Cause the current particle to bounce in the Y direction */
   bounceY() {
     this.vy = -1 * this.vy;
   }
 }
 
 
-// Orchestrates and renders particles
+/** Orchestrates and renders particles */
 class ParticleRenderer {
+  /**
+   * Create a new particle renderer.
+   * @param {CanvasRenderingContext2D} ctx - the rendering context of the canvas to draw on
+   * @param {number} particleCount - the number of particles to maintain in the simulation
+   */
   constructor(ctx, particleCount) {
     this.ctx = ctx;
     this.particleCount = particleCount;
@@ -69,10 +87,18 @@ class ParticleRenderer {
     ));
   }
 
+  /**
+   * Get the width of the canvas coordinate system.
+   * @return {number} the width of the canvas
+   */
   width() {
     return this.ctx.canvas.width;
   }
 
+  /**
+   * Get the height of the canvas coordinate system
+   * @return {number} the height of the canvas
+   */
   height() {
     return this.ctx.canvas.height;
   }
