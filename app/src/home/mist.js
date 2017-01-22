0517846c32fb2eefd7a6e324096212fcb69c7b08
Luke Deen Taylor <luke@deentaylor.com>
2017-01-22 13:47:10 -0500

    Create particle class

--- app/src/home/mist.js
+++ app/src/home/mist.js
@@ -1,6 +1,41 @@
 const canvas = document.getElementById('mist');
 const ctx = canvas.getContext('2d');
 
+class Particle {
+  constructor(renderer, x, y, vx, vy) {
+    this.renderer = renderer;
+    this.x = x;     // X position on canvas
+    this.y = y;    // Y position on canvas
+    this.vx = vx;  // X velocity
+    this.vy = vy;  // Y velocity
+  }
+
+  step() {
+    // Bounce particles when they hit the edges of the canvas
+    if (this.x < 0 || this.x >= this.renderer.width) {
+      this.bounceX();
+    }
+    if (this.y < 0 || this.y >= this.renderer.height) {
+      this.bounceY();
+    }
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  bounceX() {
+    this.vx = -1 * this.vx;
+  }
+
+  bounceY() {
+    this.vy = -1 * this.vy;
+  }
+}
+
+class ParticleRenderer {
+
+}
+
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight * 0.8;
