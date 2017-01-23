13e6826372b5fbc1ebcf78ec3152da6750bb2186
Luke Deen Taylor <luke@deentaylor.com>
2017-01-22 23:40:13 -0500

    Remove 'Hi' text and gradient fade

--- app/src/home/mist.js
+++ app/src/home/mist.js
@@ -181,21 +181,6 @@ window.addEventListener('resize', () => {
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight * 0.8;
-
-  ctx.fillStyle = 'white';
-  ctx.font = `${canvas.height}px Arial`;
-  ctx.fillText('Hi', 10, canvas.height);
-
-
-  // Make everything faded with a gradient
-
-  ctx.globalCompositeOperation = 'destination-in';
-
-  const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-  gradient.addColorStop(0, 'rgba(255, 255, 255, 0)');
-  gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
-  ctx.fillStyle = gradient;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
 });
 
 window.addEventListener('load', () => window.dispatchEvent(new Event('resize')));
