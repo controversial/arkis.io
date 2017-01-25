88e0da7e10e34710c1387871bb7be19a15d4872f
Luke Deen Taylor <luke@deentaylor.com>
2017-01-25 00:37:05 -0500

    New webpack config to isolate builds of each entry point
    The home files are now in a home/ subdirectory, this will require modified NGINX config

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,36 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
+    // These names MUST match the folder name within app/src so that the JS bundle doesn't end up in
+    // a different directory from the files copied by file-loader
     home: path.join(__dirname, 'app/src/home'),
+    404: path.join(__dirname, 'app/src/404'),
   },
 
   module: {
     loaders: [
+      // SASS files
       {
         test: /\.sass$/,
         loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+      // JavaScript files
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: 'node_modules',
         query: { presets: ['es2015'] },
       },
-      {
-        test: /\.(ttf|woff|woff2)$/,
-        loader: 'file',
-        query: { name: 'fonts/[name].[ext]' },
-      },
-      {
-        test: /\.png$/,
-        loader: 'file',
-      },
+      // HTML files
       {
         test: /\.html$/,
-        loader: 'file-loader?name=[name].[ext]!extract-loader!html-loader',
+        loader: 'file-loader?name=[path][name].[ext]&context=app/src!extract-loader!html-loader',
+      },
+      // Files that require no compilation or processing
+      {
+        test: /\.(ttf|woff|woff2|png)$/,
+        loader: 'file',
+        query: { name: '[path][name].[ext]', context: 'app/src' },
       },
     ],
   },
@@ -37,7 +40,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'app/build'),
     publicPath: '/',
-    filename: '[name].js',
+    filename: '[name]/index.js',
   },
 
   plugins: [
