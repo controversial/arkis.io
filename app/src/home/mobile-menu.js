f054824f9225cb1d11310d32016fc6bab6f28175
Luke Deen Taylor <luke@deentaylor.com>
2017-01-28 18:34:58 -0500

    Fade in mobile menu links one by one

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -7,5 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
   menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('open');
     menu.classList.toggle('open');
+
+    const menuLinks = menu.getElementsByTagName('a');
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      setTimeout(
+        () => { menuLinks[i].classList.toggle('visible'); },
+        // Use staggered delay when opening, 0 delay when closing
+        menu.classList.contains('open') ? 250 * i : 0,
+      );
+    }
   });
 });
