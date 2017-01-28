deb9179b4cee2bfc5f4510b4ce7e8e6857685419
Luke Deen Taylor <luke@deentaylor.com>
2017-01-28 15:08:25 -0500

    Begin styling mobile menu

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -2,7 +2,10 @@ require('./mobile-menu.sass');
 
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.top-bar button.ion-md-menu');
+  const menu = document.querySelector('.mobile-menu');
+
   menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('open');
+    menu.classList.toggle('open');
   });
 });
