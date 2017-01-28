d04b7d156f313d5b8f2d8e38309343e79969665f
Luke Deen Taylor <luke@deentaylor.com>
2017-01-28 11:46:06 -0500

    Remove hamburger button from top-bar div
    But position it as if it was still there

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.querySelector('.top-bar button.ion-md-menu');
+  const menuButton = document.querySelector('button.ion-md-menu');
   menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('open');
   });
