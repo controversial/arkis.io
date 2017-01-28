fdef7180df7565e4aefe66a2462c0be7609817f9
Luke Deen Taylor <luke@deentaylor.com>
2017-01-28 11:47:00 -0500

    Add a mobile-menu.sass file

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -1,3 +1,5 @@
+require('./mobile-menu.sass');
+
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('button.ion-md-menu');
   menuButton.addEventListener('click', () => {
