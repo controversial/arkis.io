1a14b2efeb63399c330adc84d22d8d5c44596986
Luke Deen Taylor <luke@deentaylor.com>
2017-01-28 11:09:38 -0500

    Use material menu icon instead of iOS

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.querySelector('.top-bar button.ion-ios-menu');
+  const menuButton = document.querySelector('.top-bar button.ion-md-menu');
   menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('open');
   });
