7d0e8849113bd1f8ab7c1c7942c3a4cace0cac63
Luke Deen Taylor <luke@deentaylor.com>
2017-02-04 00:44:43 -0500

    Improve contact form code readability

--- app/src/home/contact.js
+++ app/src/home/contact.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // The <form> element
   const contactForm = document.getElementById('contact-form');
+  // The button in the <form> element
   const formSubmitButton = document.getElementById('contact-form-submit');
+
   formSubmitButton.addEventListener('click', () => {
-    fetch(contactForm.getAttribute('action'), {
-      method: contactForm.getAttribute('method'),
-      body: new FormData(contactForm),
-    }).then(response => response.text()).then((body) => {
-      if (body === 'true') {
-        contactForm.innerHTML = 'Success :D';
-      } else {
-        contactForm.innerHTML = 'Failure :/';
-      }
-    });
+    // Read URL and HTTP method from the attributes on the <form> element
+    const submitURL = contactForm.getAttribute('action');
+    const submitMethod = contactForm.getAttribute('method');
+    // Read data from the form for easy POSTing
+    const data = new FormData(contactForm);
+    // Perform a POST request manually instead of using a submit button. This way, the submission
+    // can happen in the background, and the user does not have to be redirected.
+    fetch(submitURL, { method: submitMethod, body: data })
+      .then(response => response.text())
+      .then((body) => {
+        if (body === 'true') { // Email sent successfully
+          contactForm.innerHTML = 'Success :D';
+        } else { // Email failed to send
+          contactForm.innerHTML = 'Failure :/';
+        }
+      });
   });
 });
