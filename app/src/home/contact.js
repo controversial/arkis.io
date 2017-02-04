97e14bcf5f0eca4b1edaa70cfe01ddeb32dfba3f
Luke Deen Taylor <luke@deentaylor.com>
2017-02-04 01:03:33 -0500

    Split submission into a separate function

--- app/src/home/contact.js
+++ app/src/home/contact.js
@@ -1,25 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+function submit() {
   // The <form> element
   const contactForm = document.forms['contact-form'];
+
+  // Read URL and HTTP method from the attributes on the <form> element
+  const submitURL = contactForm.getAttribute('action');
+  const submitMethod = contactForm.getAttribute('method');
+
+  // Read data from the form for easy POSTing
+  const data = new FormData(contactForm);
+
+  // Perform a POST request manually instead of using a submit button. This way, the submission
+  // can happen in the background, and the user does not have to be redirected.
+  fetch(submitURL, { method: submitMethod, body: data })
+    .then(response => response.text())
+    .then((body) => {
+      if (body === 'true') { // Email sent successfully
+        contactForm.innerHTML = 'Success :D';
+      } else { // Email failed to send
+        contactForm.innerHTML = 'Failure :/';
+      }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   // The button in the <form> element
   const formSubmitButton = document.getElementById('contact-form-submit');
-
-  formSubmitButton.addEventListener('click', () => {
-    // Read URL and HTTP method from the attributes on the <form> element
-    const submitURL = contactForm.getAttribute('action');
-    const submitMethod = contactForm.getAttribute('method');
-    // Read data from the form for easy POSTing
-    const data = new FormData(contactForm);
-    // Perform a POST request manually instead of using a submit button. This way, the submission
-    // can happen in the background, and the user does not have to be redirected.
-    fetch(submitURL, { method: submitMethod, body: data })
-      .then(response => response.text())
-      .then((body) => {
-        if (body === 'true') { // Email sent successfully
-          contactForm.innerHTML = 'Success :D';
-        } else { // Email failed to send
-          contactForm.innerHTML = 'Failure :/';
-        }
-      });
-  });
+  formSubmitButton.addEventListener('click', submit);
 });
