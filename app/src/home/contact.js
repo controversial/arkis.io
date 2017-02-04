74bb161c56ecd5890131123e35c4269df29c24f9
Luke Deen Taylor <luke@deentaylor.com>
2017-02-04 10:54:32 -0500

    Only allow form submission if all fields are valid
    also validate all elements on submit, instead of only validating
    elements when they're typed in.

--- app/src/home/contact.js
+++ app/src/home/contact.js
@@ -1,27 +1,3 @@
-function submit() {
-  // The <form> element
-  const contactForm = document.forms['contact-form'];
-
-  // Read URL and HTTP method from the attributes on the <form> element
-  const submitURL = contactForm.getAttribute('action');
-  const submitMethod = contactForm.getAttribute('method');
-
-  // Read data from the form for easy POSTing
-  const data = new FormData(contactForm);
-
-  // Perform a POST request manually instead of using a submit button. This way, the submission
-  // can happen in the background, and the user does not have to be redirected.
-  fetch(submitURL, { method: submitMethod, body: data })
-    .then(response => response.text())
-    .then((body) => {
-      if (body === 'true') { // Email sent successfully
-        contactForm.innerHTML = 'Success :D';
-      } else { // Email failed to send
-        contactForm.innerHTML = 'Failure :/';
-      }
-    });
-}
-
 function validate(input) {
   // Returns true if `value` is empty or contains only whitespace
   function isEmpty(value) {
@@ -37,16 +13,51 @@ function validate(input) {
   return true;
 }
 
+function updateValidity(input) {
+  const isValid = validate(input);
+  if (!isValid) input.classList.add('invalid');
+  else input.classList.remove('invalid');
+}
+
+function submit() {
+  // The <form> element
+  const contactForm = document.forms['contact-form'];
+
+  // Run validation on all inputs
+  const formInputs = contactForm.getElementsByTagName('input');
+  for (let i = 0; i < formInputs.length; i += 1) {
+    updateValidity(formInputs[i]);
+  }
+
+  // Make sure everything is valid
+  if (contactForm.getElementsByClassName('invalid').length === 0) {
+    // Read URL and HTTP method from the attributes on the <form> element
+    const submitURL = contactForm.getAttribute('action');
+    const submitMethod = contactForm.getAttribute('method');
+
+    // Read data from the form for easy POSTing
+    const data = new FormData(contactForm);
+
+    // Perform a POST request manually instead of using a submit button. This way, the submission
+    // can happen in the background, and the user does not have to be redirected.
+    fetch(submitURL, { method: submitMethod, body: data })
+      .then(response => response.text())
+      .then((body) => {
+        if (body === 'true') { // Email sent successfully
+          contactForm.innerHTML = 'Success :D';
+        } else { // Email failed to send
+          contactForm.innerHTML = 'Failure :/';
+        }
+      });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   // Submit when the button in the <form> element is clicked
   const formSubmitButton = document.getElementById('contact-form-submit');
   formSubmitButton.addEventListener('click', submit);
+
   // Validate each input when it's typed in
-  // TODO: visual feedback
   const contactForm = document.forms['contact-form'];
-  contactForm.addEventListener('input', (e) => {
-    const valid = validate(e.target);
-    if (!valid) e.target.classList.add('invalid');
-    else e.target.classList.remove('invalid');
-  });
+  contactForm.addEventListener('input', e => updateValidity(e.target));
 });
