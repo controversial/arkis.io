9cc54925a5e6ef11874d436ec00359df521d35bf
Luke Deen Taylor <luke@deentaylor.com>
2017-02-09 19:06:15 -0500

    Refactor mobile-menu.js

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -3,25 +3,36 @@ require('./mobile-menu.sass');
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.top-bar button.ion-md-menu');
   const menu = document.getElementById('mobile-menu');
+  const topBar = document.getElementsByClassName('top-bar')[0];
+  const menuLinks = menu.getElementsByTagName('a');
 
-  menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('open');
-    menu.classList.toggle('open');
-    document.getElementsByClassName('top-bar')[0].classList.toggle('mobile-menu-open');
+  function openMenu() {
+    menu.style.display = 'block';
+    menuButton.classList.add('open');
+    menu.classList.add('open');
+    topBar.classList.add('mobile-menu-open');
 
-    if (menu.classList.contains('open')) {
-      menu.style.display = 'block';
-    } else {
-      setTimeout(() => { menu.style.display = 'none'; }, 500);
-    }
-
-    const menuLinks = menu.getElementsByTagName('a');
     for (let i = 0; i < menuLinks.length; i += 1) {
-      setTimeout(
-        () => { menuLinks[i].classList.toggle('visible'); },
-        // Use staggered delay when opening, 0 delay when closing
-        menu.classList.contains('open') ? 250 * i : 0,
-      );
+      // Use staggered delay for fading in menu links while opening
+      setTimeout(() => { menuLinks[i].classList.add('visible'); }, 250 * i);
     }
-  });
+  }
+
+  function closeMenu() {
+    menuButton.classList.remove('open');
+    menu.classList.remove('open');
+    topBar.classList.remove('mobile-menu-open');
+    setTimeout(() => { menu.style.display = 'none'; }, 500);
+
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].classList.remove('visible');
+    }
+  }
+
+  function toggleMenu() {
+    if (menu.classList.contains('open')) closeMenu();
+    else openMenu();
+  }
+
+  menuButton.addEventListener('click', toggleMenu);
 });
