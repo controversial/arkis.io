202bb94dfe5db1c1271f6a4f8043fd479dfae1c6
Luke Deen Taylor <luke@deentaylor.com>
2017-02-09 19:14:45 -0500

    Move mobile menu functions into object

--- app/src/home/mobile-menu.js
+++ app/src/home/mobile-menu.js
@@ -2,37 +2,39 @@ require('./mobile-menu.sass');
 
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.top-bar button.ion-md-menu');
-  const menu = document.getElementById('mobile-menu');
+  const menuElem = document.getElementById('mobile-menu');
   const topBar = document.getElementsByClassName('top-bar')[0];
-  const menuLinks = menu.getElementsByTagName('a');
+  const menuLinks = menuElem.getElementsByTagName('a');
 
-  function openMenu() {
-    menu.style.display = 'block';
-    menuButton.classList.add('open');
-    menu.classList.add('open');
-    topBar.classList.add('mobile-menu-open');
+  window.mobileMenu = {
+    open: () => {
+      menuElem.style.display = 'block';
+      menuButton.classList.add('open');
+      menuElem.classList.add('open');
+      topBar.classList.add('mobile-menu-open');
 
-    for (let i = 0; i < menuLinks.length; i += 1) {
-      // Use staggered delay for fading in menu links while opening
-      setTimeout(() => { menuLinks[i].classList.add('visible'); }, 250 * i);
-    }
-  }
+      for (let i = 0; i < menuLinks.length; i += 1) {
+        // Use staggered delay for fading in menu links while opening
+        setTimeout(() => { menuLinks[i].classList.add('visible'); }, 250 * i);
+      }
+    },
 
-  function closeMenu() {
-    menuButton.classList.remove('open');
-    menu.classList.remove('open');
-    topBar.classList.remove('mobile-menu-open');
-    setTimeout(() => { menu.style.display = 'none'; }, 500);
+    close: () => {
+      menuButton.classList.remove('open');
+      menuElem.classList.remove('open');
+      topBar.classList.remove('mobile-menu-open');
+      setTimeout(() => { menuElem.style.display = 'none'; }, 500);
 
-    for (let i = 0; i < menuLinks.length; i += 1) {
-      menuLinks[i].classList.remove('visible');
-    }
-  }
+      for (let i = 0; i < menuLinks.length; i += 1) {
+        menuLinks[i].classList.remove('visible');
+      }
+    },
 
-  function toggleMenu() {
-    if (menu.classList.contains('open')) closeMenu();
-    else openMenu();
-  }
+    toggle: () => {
+      if (menuElem.classList.contains('open')) window.mobileMenu.close();
+      else window.mobileMenu.open();
+    },
+  };
 
-  menuButton.addEventListener('click', toggleMenu);
+  menuButton.addEventListener('click', window.mobileMenu.toggle);
 });
