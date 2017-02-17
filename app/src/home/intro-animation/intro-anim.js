ec80b6e633de098491ae0a1a37672d4c4eda6fbc
Luke Deen Taylor <luke@deentaylor.com>
2017-02-17 11:08:33 -0500

    Autoplay opening animation without clicking
    Closes #6

--- app/src/home/intro-animation/intro-anim.js
+++ app/src/home/intro-animation/intro-anim.js
@@ -1,9 +1,9 @@
 require('./intro-anim.sass');
 const cascadeFadeIn = require('../build');
 
-const isTouchDevice = 'ontouchstart' in document.documentElement;
-const ev = isTouchDevice ? 'touchend' : 'click';
-const delay = isTouchDevice ? 1050 : 550;
+// const isTouchDevice = 'ontouchstart' in document.documentElement;
+// const ev = isTouchDevice ? 'touchend' : 'click';
+// const delay = isTouchDevice ? 1050 : 550;
 
 
 // Timed functions for interaction with animated logo
@@ -12,17 +12,6 @@ setTimeout(() => {
   // Make the logo interactable after the initial animation is finished
 
   const logo = document.getElementById('animated-logo');
-  const cornersDiv = document.getElementById('corners');
-  cornersDiv.classList.add('interactable');
-
-  // Click event triggers further animations on the elements
-
-  cornersDiv.addEventListener(ev, () => setTimeout(() => {
-    logo.classList.add('clicked');
-    // Set display: none on logo
-    setTimeout(() => {
-      logo.style.display = 'none';
-      cascadeFadeIn();
-    }, 1000);
-  }, delay));
-}, 3750);
+  logo.style.display = 'none';
+  cascadeFadeIn();
+}, 6000);
