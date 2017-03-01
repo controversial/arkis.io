6eb02cd24312952b155f77071b861e673700b6e3
Luke Deen Taylor <luke@deentaylor.com>
2017-03-01 15:42:55 +0000

    Regex for node_modules instead of string
    I think this makes a difference but I don't really know

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: 'node_modules',
+        exclude: /node_modules/,
         query: { presets: ['es2015'] },
       },
       // Pug files
